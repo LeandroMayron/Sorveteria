40da0e9874cf920847082788ad14d53b14d6dfbb
Leandro Mayron <leandromayron9999@gmail.com>
2024-02-05 06:33:05 -0300

    Criado o componente Topo

---
+++ src/Paginas/Home/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Topo from "../../Componentes/Topo/index"
+
+export default function Home() {
+    return(
+        <div>
+            <Topo/>
+        </div>
+    );
+}
